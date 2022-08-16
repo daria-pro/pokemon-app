38eec061d8e519f231cb0038374312be96808295
daria-pro <shaulkod@gmail.com>
2022-08-16 15:37:37 +0300

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:daria-pro/pokemon-app.git main:gh-pages
+git push -f git@github.com:daria-pro/pokemon-app.git master:gh-pages
 cd -
