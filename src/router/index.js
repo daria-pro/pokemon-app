4491223be1b94b42fef0017d3461c244552929a8
shaulkodaria <shaulko.daria@yodagu.com>
2022-08-16 10:09:41 +0300

    added pokemon component

--- src/router/index.js
+++ src/router/index.js
@@ -8,13 +8,17 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/pokemon-home',
+    name: 'pokemon-home',
+    component: () => import('../views/PokemonHome.vue'),
+    children: [
+      {
+        path: '/pokemon-app/:slug',
+        name: 'pokemon',
+        component: () => import('../views/PokemonView.vue')
+      }
+    ]
+  },
 ]
 
 const router = createRouter({
---
+++ src/views/PokemonView.vue
@@ -0,0 +1,5 @@
+<template>
+ <h1>This is Pokemon page</h1>
+ <h3>id: {{$route.params.slug}}</h3>
+</template>
+
